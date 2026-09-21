353d113efa39a38edc48b4886dd4d6a118510fd0
RadoslavL <rlelchev@abv.bg>
2026-09-21 03:54:10 +0300

    Fixed missing video information for collaboration videos

--- src/invidious/yt_backend/extractors.cr
+++ src/invidious/yt_backend/extractors.cr
@@ -662,14 +662,26 @@ private module Parsers
         metadata = item_contents.dig("metadata", "lockupMetadataViewModel")
         title = metadata.dig("title", "content").as_s
         # Contains the views of the video and the published time of the video.
-        metadata_parts = metadata.dig("metadata", "contentMetadataViewModel", "metadataRows", 0, "metadataParts").try &.as_a
+        metadata_rows = metadata.dig("metadata", "contentMetadataViewModel", "metadataRows").try &.as_a
 
-        view_count_text = metadata_parts.try &.find { |item| item["icon"]?.nil? && item.dig?("text", "content").try &.as_s.includes?("views") }
-          .try &.dig("text", "content").as_s
-        published = metadata_parts.try &.find { |item| item["icon"]?.nil? && item.dig?("text", "content").try &.as_s.includes?("ago") }
-          .try { |item| decode_date(item.dig("text", "content").as_s) } || Time.local
+        view_count_text : String? = nil
+        published : Time = Time.local
+        view_count : Int64 = 0_i64
 
-        view_count = short_text_to_number(view_count_text || "0")
+        metadata_rows.each do |row|
+          metadata_parts = row.dig?("metadataParts").try &.as_a
+          next unless metadata_parts
+
+          view_count_text = metadata_parts.try &.find { |item| item["icon"]?.nil? && item.dig?("text", "content").try &.as_s.includes?("views") }
+            .try &.dig("text", "content").as_s
+          published = metadata_parts.try &.find { |item| item["icon"]?.nil? && item.dig?("text", "content").try &.as_s.includes?("ago") }
+            .try { |item| decode_date(item.dig("text", "content").as_s) } || Time.local
+
+          view_count = short_text_to_number(view_count_text || "0")
+          if view_count_text
+            break
+          end
+        end
 
         length = thumbnail_view_model.dig("overlays", 0, "thumbnailBottomOverlayViewModel", "badges", 0, "thumbnailBadgeViewModel", "text").try &.as_s
 
